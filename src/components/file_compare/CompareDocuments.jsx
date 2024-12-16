aeb22ed526a42ab4adc3000f310c658d8b7ae1a0
Aleksandr <gggols32@gmail.com>
2024-12-16 12:47:25 +0300

    Add navigate action on file upload

--- src/components/file_compare/CompareDocuments.jsx
+++ src/components/file_compare/CompareDocuments.jsx
@@ -1,10 +1,18 @@
 
+import { useNavigate } from "react-router-dom";
 import { FileUpload } from 'primereact/fileupload';
 
 function CompareDocuments() {
+    const navigate = useNavigate()
+
+    const onUpload = (files) => {
+        const data = JSON.parse(files.xhr.responseText)
+        navigate(`/view/${data["file_compare"]}`)
+    }
+
     return (
         <div className="card mt-3">
-            <FileUpload name="demo[]" url={'/api/upload'} multiple accept="application/pdf" emptyTemplate={<p className="m-0">Добавьте файлы, перетащив их в данную область</p>} chooseLabel='Добавить' uploadLabel='Отправить' cancelLabel='Сбросить'/>
+            <FileUpload name="files" url='http://localhost:5000/compare' multiple accept="application/pdf" emptyTemplate={<p className="m-0">Добавьте файлы, перетащив их в данную область</p>} chooseLabel='Добавить' uploadLabel='Отправить' cancelLabel='Сбросить' onUpload={onUpload}/>
         </div>
     )
 }
