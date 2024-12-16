67a7aa52e08c56d0f607a0be5455071bad57ea7b
Aleksandr <gggols32@gmail.com>
2024-12-16 16:44:40 +0300

    API Service URL getting from .env / Update .gitignore / Add .env.example

--- src/components/file_compare/CompareDocuments.jsx
+++ src/components/file_compare/CompareDocuments.jsx
@@ -4,6 +4,7 @@ import { FileUpload } from 'primereact/fileupload';
 
 function CompareDocuments() {
     const navigate = useNavigate()
+    const uploadUrl = new URL("extract_facts", import.meta.env.VITE_COMPARE_SERVICE_URL)
 
     const onUpload = (files) => {
         const data = JSON.parse(files.xhr.responseText)
@@ -12,7 +13,7 @@ function CompareDocuments() {
 
     return (
         <div className="card mt-3">
-            <FileUpload name="files" url='http://localhost:5000/extract_facts' multiple accept="application/pdf" emptyTemplate={<p className="m-0">Добавьте файлы, перетащив их в данную область</p>} chooseLabel='Добавить' uploadLabel='Отправить' cancelLabel='Сбросить' onUpload={onUpload}/>
+            <FileUpload name="files" url={uploadUrl} multiple accept="application/pdf" emptyTemplate={<p className="m-0">Добавьте файлы, перетащив их в данную область</p>} chooseLabel='Добавить' uploadLabel='Отправить' cancelLabel='Сбросить' onUpload={onUpload}/>
         </div>
     )
 }
--- src/services/FileCompareService.js
+++ src/services/FileCompareService.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 class FileCompareService{
     static get_file_compares() {
-        const url = "http://localhost:5000/file_compares";
+        console.log(import.meta.env)
+        const url = new URL("file_compares", import.meta.env.VITE_COMPARE_SERVICE_URL);
         return axios.get(url).then((res) => res.data);
     }
 }
