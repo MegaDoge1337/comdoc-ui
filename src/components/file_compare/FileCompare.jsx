6c478d74beb0cb7e338ab76df05d5b59c94f9a3e
Aleksandr <gggols32@gmail.com>
2024-12-17 18:33:51 +0300

    Theme changed / Change logic of compare results api call / Add TabPanels for table view and separate documents view

--- src/components/file_compare/FileCompare.jsx
+++ src/components/file_compare/FileCompare.jsx
@@ -2,6 +2,8 @@ import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import FileCompareService from "../../services/FileCompareService";
 
+import { TabView, TabPanel } from 'primereact/tabview';
+import { Divider } from 'primereact/divider';
 import { ProgressSpinner } from 'primereact/progressspinner';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
@@ -14,13 +16,14 @@ function FileCompare() {
     const params = useParams();
 
     useEffect(() => {
-        let intervalId;
+        let timeout = 5000;
+        let timeoutId;
 
         const checkProcessing = async () => {
             const result = await FileCompareService.check_processing(params.id);
             if (result.is_done) {
                 setIsDone(true);
-                clearInterval(intervalId);
+                clearTimeout(timeoutId);
 
                 const facts = await FileCompareService.get_compared_facts(params.id);
                 if(facts) {
@@ -29,18 +32,23 @@ function FileCompare() {
                     setFacts(facts);
                 }
             }
+            else 
+            {
+                clearTimeout(timeoutId);
+                setTimeout(checkProcessing, timeout)
+            }
         };
 
-        intervalId = setInterval(checkProcessing, 2000);
+        timeoutId = setTimeout(checkProcessing, timeout);
 
         return () => {
-            clearInterval(intervalId);
+            clearTimeout(timeoutId);
         };
     }, [params.id]);
 
     const rowClass = (data) => {
         return {
-            'bg-red-600': !data.is_equals
+            'bg-red-300': !data.is_equals
         };
     };
 
@@ -48,12 +56,23 @@ function FileCompare() {
         return (
             <div className="card mt-3">
                 <Card title={`${facts.fist_file_name} / ${facts.second_file_name}`}>
-                    <DataTable value={facts.facts} sortMode="multiple" tableStyle={{ minWidth: '50rem' }} rowClassName={rowClass}>
-                        <Column field="fact_localization" header="Имя факта" sortable style={{ width: '25%' }}></Column>
-                        <Column field="line_number" header="№ Линии" sortable style={{ width: '25%' }}></Column>
-                        <Column field="f_value" header="Факт №1" sortable style={{ width: '25%' }}></Column>
-                        <Column field="s_value" header="Факт №2" sortable style={{ width: '25%' }}></Column>
-                    </DataTable>
+                    <TabView>
+                        <TabPanel header="Таблица фактов">
+                            <DataTable value={facts.facts} sortMode="multiple" tableStyle={{ minWidth: '50rem' }} rowClassName={rowClass}>
+                                <Column field="fact_localization" header="Имя факта" sortable style={{ width: '25%' }}></Column>
+                                <Column field="line_number" header="№ Линии" sortable style={{ width: '25%' }}></Column>
+                                <Column field="f_value" header="Факт №1" sortable style={{ width: '25%' }}></Column>
+                                <Column field="s_value" header="Факт №2" sortable style={{ width: '25%' }}></Column>
+                            </DataTable>
+                        </TabPanel>
+                        <TabPanel header="Документы">
+                            <div className="card flex justify-content-center">
+                                <div>Первый документ</div>
+                                <Divider layout="vertical" />
+                                <div>Второй документ</div>
+                            </div>
+                        </TabPanel>
+                    </TabView>
                 </Card>
             </div>
         )
@@ -61,7 +80,7 @@ function FileCompare() {
 
     if(isDone && !isFactsLoaded) {
         return (
-            <div className="flex flex-column justify-content-center align-items-center align-content-center mt-3">
+            <div className="flex flex-column justify-content-center align-items-center align-content-center mt-5">
                 <ProgressSpinner/>
                 <p>Проводим сравнение</p>
             </div>
@@ -69,7 +88,7 @@ function FileCompare() {
     }
 
     return (
-        <div className="flex flex-column justify-content-center align-items-center align-content-center mt-3">
+        <div className="flex flex-column justify-content-center align-items-center align-content-center mt-5">
             <ProgressSpinner/>
             <p>Извлекаем факты</p>
         </div>
