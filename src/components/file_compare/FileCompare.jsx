3b8fbf7ae86b6f4d79bfa2e8a5d9fc7acdf9d46e
Aleksandr <gggols32@gmail.com>
2024-12-17 13:20:19 +0300

    Add table view for single comparison

--- src/components/file_compare/FileCompare.jsx
+++ src/components/file_compare/FileCompare.jsx
@@ -1,12 +1,77 @@
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import FileCompareService from "../../services/FileCompareService";
+
+import { ProgressSpinner } from 'primereact/progressspinner';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { Card } from 'primereact/card';
 
 function FileCompare() {
-
+    const [isDone, setIsDone] = useState(false);
+    const [isFactsLoaded, setIsFactsLoaded] = useState(false);
+    const [facts, setFacts] = useState([]);
     const params = useParams();
 
+    useEffect(() => {
+        let intervalId;
+
+        const checkProcessing = async () => {
+            const result = await FileCompareService.check_processing(params.id);
+            if (result.is_done) {
+                setIsDone(true);
+                clearInterval(intervalId);
+
+                const facts = await FileCompareService.get_compared_facts(params.id);
+                if(facts) {
+                    setIsFactsLoaded(true);
+                    console.log(facts)
+                    setFacts(facts);
+                }
+            }
+        };
+
+        intervalId = setInterval(checkProcessing, 2000);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, [params.id]);
+
+    const rowClass = (data) => {
+        return {
+            'bg-red-600': !data.is_equals
+        };
+    };
+
+    if(isDone && isFactsLoaded) {
+        return (
+            <div className="card mt-3">
+                <Card title={`${facts.fist_file_name} / ${facts.second_file_name}`}>
+                    <DataTable value={facts.facts} sortMode="multiple" tableStyle={{ minWidth: '50rem' }} rowClassName={rowClass}>
+                        <Column field="fact_localization" header="Имя факта" sortable style={{ width: '25%' }}></Column>
+                        <Column field="line_number" header="№ Линии" sortable style={{ width: '25%' }}></Column>
+                        <Column field="f_value" header="Факт №1" sortable style={{ width: '25%' }}></Column>
+                        <Column field="s_value" header="Факт №2" sortable style={{ width: '25%' }}></Column>
+                    </DataTable>
+                </Card>
+            </div>
+        )
+    }
+
+    if(isDone && !isFactsLoaded) {
+        return (
+            <div className="flex flex-column justify-content-center align-items-center align-content-center mt-3">
+                <ProgressSpinner/>
+                <p>Проводим сравнение</p>
+            </div>
+        )
+    }
+
     return (
-        <div className="card mt-3">
-            {params.id}
+        <div className="flex flex-column justify-content-center align-items-center align-content-center mt-3">
+            <ProgressSpinner/>
+            <p>Извлекаем факты</p>
         </div>
     );
 }
--- src/services/FileCompareService.js
+++ src/services/FileCompareService.js
@@ -1,9 +1,18 @@
 import axios from "axios";
 
 class FileCompareService{
-    static get_file_compares() {
-        console.log(import.meta.env)
+    static async get_file_compares() {
         const url = new URL("file_compares", import.meta.env.VITE_COMPARE_SERVICE_URL);
+        return axios.get(url).then((res) => res.data);
+    }
+
+    static async check_processing(file_compare_id) {
+        const url = new URL(`check_processing/${file_compare_id}`, import.meta.env.VITE_COMPARE_SERVICE_URL);
+        return axios.get(url).then((res) => res.data);
+    }
+
+    static async get_compared_facts(file_compare_id) {
+        const url = new URL(`compare_facts/${file_compare_id}`, import.meta.env.VITE_COMPARE_SERVICE_URL);
         return axios.get(url).then((res) => res.data);
     }
 }
